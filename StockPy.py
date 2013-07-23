0b3b019d06e5aa7b21881fabf4d0a13bfba6a300
Narasimha Prasad Prabhu <narasimha.89@gmail.com>
2013-07-23 18:39:51 +0800

    Complete restructure of code to follow pep8 guideline
    Also incroporated as fucntions to make code more clear

--- StockPy.py
+++ StockPy.py
@@ -1,90 +1,76 @@
-import matplotlib.pyplot as plt
+import pandas.io.data as web
+import datetime as dt
+import pandas as pd
 import numpy as np
-from pandas import rolling_mean
-from pandas.io.data import DataReader
-from datetime import date
-from datetime import datetime
-from matplotlib.widgets import MultiCursor
-from matplotlib.widgets import RadioButtons
+import matplotlib.pyplot as plt
+import matplotlib.widgets as wd
 
-#Setup figure. 3 plots. Top: Closing Share Values. Bottom(Bot): Trading Volumes
-#Side(Sid): Histogram of day to day variation in stock prices
-fig = plt.figure()
-top = plt.subplot(221)
-bot = plt.subplot(223,sharex=top)
-sid = plt.subplot(122)
+if __name__ == "__main__":
+    #Setup figure
+    #Top, Bottom, Side with top and bottom plot sharing x axis
+    fig = plt.figure()
+    top = plt.subplot(221)
+    bot = plt.subplot(223, sharex=top)
+    sid = plt.subplot(122)
 
-#Default: GOOG(Google)
-stockname = "GOOG"
-stock = DataReader(stockname,"yahoo",date(2007,1,1))
+    def plot_data(stkname, fig, topplt, botplt, sidplt):
+        #Get data from yahoo
+        #Calculate olling mean, mean and current value of stock
+        #Also calculate length of data
+        startdate = dt.date(2007, 1, 1)
+        stkdata = web.DataReader(stkname, 'yahoo', startdate)
+        stklen = len(stkdata.index)
+        enddate = dt.datetime.date(stkdata.index[stklen-1])
+        stkrolmean = pd.rolling_mean(stkdata['Close'], 60)
+        stkmean = stkdata['Close'].mean(1)
+        stkcur = stkdata['Close'][stklen-1]
 
-#Prop for showing the latest price    
-props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+        #Decoration for annotation of latest trading value
+        props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 
-#Top plot. Contains closing price, the mean and running mean
-top.plot(stock.index,stock['Close'],stock.index,
-         stock['Close'].mean(1)*np.ones(len(stock.index)),stock.index,
-            rolling_mean(stock['Close'],60))
-top.set_title('%s Stock Price from %s to %s' %(stockname,date(2007,1,1),
-    datetime.date(stock.index[len(stock.index)-1])))
-top.grid(True)
-top.text(0.05, 0.95, 'Trading Price on %s : $%s' 
-    %(stock.index[len(stock.index)-1], stock['Close'][len(stock.index)-1]),
-    transform=top.transAxes,fontsize=14,verticalalignment='top',bbox=props)
-ymin,ymax = top.get_ylim()
-top.fill_between(stock.index,stock['Close'],
-                 (ymin+0.05)*np.ones(len(stock.index)),alpha=0.5)
+        #Top plot: Closing data, mean and rolling mean
+        topplt.plot(stkdata.index, stkdata['Close'], stkdata.index,
+                    stkmean*np.ones(stklen), stkdata.index, stkrolmean)
+        topplt.set_title('{} Stock Price from {} to {}'.format(stkname,
+                         startdate, enddate))
+        topplt.grid(True)
+        topymin, topymax = topplt.get_ylim()
+        topplt.text(0.05, 0.95, 'Trading price on {}: ${}'.format(enddate,
+                    stkcur), transform=top.transAxes, fontsize=14,
+                    verticalalignment='top', bbox=props)
+        topplt.fill_between(stkdata.index, stkdata['Close'],
+                            (topymin+0.1)*np.ones(stklen), alpha=0.5)
 
-#Bottom plot contains the trading volumes of the stock  
-bot.bar(stock.index,stock['Volume'])
-bot.set_title('%s Trading Volume' %(stockname))
+        #Bottom plot: Bar Graph, trading volume
+        botplt.bar(stkdata.index, stkdata['Volume'])
+        botplt.set_title('{} Trading Volume'.format(stkname))
 
-#Side plot contains the histogram of stock value variation
-n,bins,patches = sid.hist(stock['High']-stock['Low'],bins=50,normed=True)
-sid.set_title('Stock Value Variation')
-sid.grid(True)
+        #Side plot: histogram of 'high-low'
+        sidplt.hist(stkdata['High']-stkdata['Low'], bins=50, normed=True)
+        sidplt.set_title('Stock Value Variation')
+        sidplt.grid(True)
 
-#Remove x axis tick on top plot
-#Set tight layout
-#Setup multicursor on top and bottom plot
-plt.setp(top.get_xticklabels(),visible=False)
-plt.tight_layout()
-multi = MultiCursor(fig.canvas,(top,bot),color='r',lw=2)
+        #Remove xticklabels on top plot
+        plt.setp(top.get_xticklabels(), visible=True)
+        plt.tight_layout()
+        return fig
 
-#Setup for radio bottoms
-axcolor = 'lightgoldenrodyellow'
-prop_radio = plt.axes([0.85, 0.85, 0.1, 0.1], axisbg=axcolor)
-radio = RadioButtons(prop_radio,('GOOG','MSFT','YHOO','GE'))
-def stocksel(label):
-    top.cla()
-    bot.cla()
-    sid.cla()
+    fig = plot_data('GOOG', fig, top, bot, sid)
+    multi = wd.MultiCursor(fig.canvas, (top, bot), color='r', lw=2)
 
-    stockname = label
-    stock = DataReader(stockname,"yahoo",date(2007,1,1))
-        
-    top.plot(stock.index,stock['Close'],stock.index,
-         stock['Close'].mean(1)*np.ones(len(stock.index)),stock.index,
-            rolling_mean(stock['Close'],60))
-    top.set_title('%s Stock Price from %s to %s' %(stockname,date(2007,1,1),
-        datetime.date(stock.index[len(stock.index)-1])))
-    top.grid(True)
-    top.text(0.05, 0.95, 'Trading Price on %s : $%s' 
-        %(stock.index[len(stock.index)-1], stock['Close'][len(stock.index)-1]),
-        transform=top.transAxes,fontsize=14,verticalalignment='top',bbox=props)
-    ymin,ymax = top.get_ylim()
-    top.fill_between(stock.index,stock['Close'],
-                     (ymin+0.05)*np.ones(len(stock.index)),alpha=0.5)
-      
-    bot.bar(stock.index,stock['Volume'])
-    bot.set_title('%s Trading Volume' %(stockname))
-    
-    n,bins,patches = sid.hist(stock['High']-stock['Low'],bins=50,normed=True)
-    sid.set_title('Stock Value Variation')
-    sid.grid(True)
-    
-    plt.setp(top.get_xticklabels(),visible=False)
-    plt.tight_layout()
-radio.on_clicked(stocksel)
+    #Setup for radio bottoms
+    axcolor = 'lightgoldenrodyellow'
+    prop_radio = plt.axes([0.95, 0.9, 0.05, 0.1], axisbg=axcolor)
+    radio = wd.RadioButtons(prop_radio, ('GOOG', 'MSFT', 'YHOO', 'GE'))
 
-plt.show()
+    def stocksel(label):
+        #Clear all axes
+        top.cla()
+        bot.cla()
+        sid.cla()
+
+        plot_data(label, fig, top, bot, sid)
+    radio.on_clicked(stocksel)
+
+    #Show plot
+    plt.show()
